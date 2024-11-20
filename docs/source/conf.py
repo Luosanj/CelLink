e9c1d39edc85fa04c1af1e9c5d421f7b38dfc27e
XIN LUO <46926952+Luosanj@users.noreply.github.com>
2024-11-19 23:00:47 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,26 +14,34 @@ release = '0.1.3'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-templates_path = ['_templates']
-exclude_patterns = []
-
-language = 'English'
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
 extensions = [
     'nbsphinx',
-    'sphinx.ext.mathjax',  # For rendering equations
-    'sphinx.ext.viewcode', # Optional, for linking code
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
 
-# Exclude build directory
-exclude_patterns = ['_build', '**.ipynb_checkpoints']
-source_suffix = ['.rst', '.ipynb']
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+def setup(app):
+    app.add_css_file('my_theme.css')
+
+html_static_path = ['_static'] 
 
 # Set notebooks to render on a specific kernel (if needed)
 nbsphinx_kernel_name = 'python3'
