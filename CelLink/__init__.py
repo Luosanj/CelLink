200adc7ddca55e5e169e3684ecda4611d155b863
Haoxuan Zeng <zchx@lh-login2.arc-ts.umich.edu>
2025-09-08 01:27:10 -0400

    Update api doc

--- CelLink/__init__.py
+++ CelLink/__init__.py
@@ -1,5 +1,9 @@
 __version__ = "0.1.4"
 
-from src.cellink.model import Cellink
+# Import the main class and submodules using relative imports so the package can be
+# imported by Sphinx/autosummary during doc builds.
+from .model import Cellink
 from . import utils
 from . import metrics
+
+__all__ = ["Cellink", "utils", "metrics"]
